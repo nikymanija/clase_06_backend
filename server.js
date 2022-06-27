8744276bb9117ec89f0e0064450728db1893b932
Nicolas Silva <nicosilva1992@hotmail.com>
2022-06-27 00:50:32 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,25 @@
+
+const express = require('express')
+const app = express()
+const PORT = 8080
+let visitas=0  
+
+app.get('/', (request,response)=>{
+    response.send({mensaje:'Saludos a mi amigo Martin!'})
+})
+
+
+app.get('/visita',(req,res)=>{
+  
+visitas++
+res.send(`Este sitio tuvo${visitas} totales`)
+})
+
+
+
+
+const server = app.listen(PORT,()=>{
+    console.log(`Server Listenig [${PORT}]...`);
+})
+
+server.on('error', e=>console.log(`Error On Server`,e))
