9015468365b13f2a49bd85760571c97d943ef7f5
Nicolas Silva <nicosilva1992@hotmail.com>
2022-06-29 09:47:22 -0300

    first commit

--- server.js
+++ server.js
@@ -9,11 +9,11 @@ let contenedor = new Contenedor("productos.txt");
 
 function randomFun (min,max)
 { 
-  return Math.floor(Math.random()* (max - min) + min);
+  return Math.floor(Math.random()* (max - min + 1 )) + min;
   
 }
 
-resRandom = randomFun(2,3);
+
 
 
 app.get("/productos", async (request, response) => {
@@ -23,7 +23,8 @@ app.get("/productos", async (request, response) => {
 
 app.get("/productosRandom", async (request, response) => {
   await contenedor.init();
-  response.json(contenedor.getById(resRandom));
+  const products = contenedor.getAll();
+  response.json(contenedor.getById(randomFun(1,products.length)));
 });
 
 const server = app.listen(PORT, () => {
